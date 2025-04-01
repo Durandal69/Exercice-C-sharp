d099211cd0a8b5b7c212db0b9a367a0ad69e4980
Durandal69 <stanislas.fleutot@gmail.com>
2025-04-01 17:29:26 +0200

    Mise à jour mardi soir
    Fin de la journée de mardi

--- ExerciceEquationSecondDegre/Program.cs
+++ ExerciceEquationSecondDegre/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Jour 2 Exercice 6 : Equation du second degré
 
+// Les Variables
+// VARIABLE discriminant, a, b, c, x1, x2 = REEL
 
+// AFFICHER "Quelle est votre coefficient a ?"
+// LIRE a
+// AFFICHER "Quelle est votre coefficient b ?"
+// LIRE b
+// AFFICHER "Quelle est votre coefficient c ?"
+// LIRE c
+
+// discriminant <- b² - 4 * a * c
+
+// SI discriminant < 0
+// AFFICHER "Pas de solution réelle"
+// SINON SI discriminant == 0
+// x1 <- -b / (2 * a)
+// AFFICHER "x1"
