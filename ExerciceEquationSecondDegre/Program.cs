75b82b5f1d2fa3f110d84df58be8f8e4dfdb091f
Durandal69 <stanislas.fleutot@gmail.com>
2025-04-02 08:54:49 +0200

    Ajout de l'exercice Equation du Second Degré
    exercice EquationSecondDegre terminé

--- ExerciceEquationSecondDegre/Program.cs
+++ ExerciceEquationSecondDegre/Program.cs
@@ -3,6 +3,7 @@
 // Les Variables
 // VARIABLE discriminant, a, b, c, x1, x2 = REEL
 
+// AFFICHER "Résolution de l'équation ax² + bx + c = 0."
 // AFFICHER "Quelle est votre coefficient a ?"
 // LIRE a
 // AFFICHER "Quelle est votre coefficient b ?"
@@ -13,7 +14,39 @@
 // discriminant <- b² - 4 * a * c
 
 // SI discriminant < 0
-// AFFICHER "Pas de solution réelle"
+// AFFICHER $"L'équation {a}x² + {b}x + 2 = 0 n'a pas de solution réelle."
 // SINON SI discriminant == 0
 // x1 <- -b / (2 * a)
-// AFFICHER "x1"
+// AFFICHER $"L'équation {a}x² + {b}x + 2 = 0 a une solution réelle {x1}."
+// SINON SI discriminant > 0
+// x1 <- (-b - √discriminant) / (2 * a)
+// x2 <- (-b + √discriminant) / (2 * a)
+// AFFICHER $"L'équation {a}x² + {b}x + 2 = 0 a deux solutions réelles {x1} ou {x2}."
+
+double discriminant, a, b, c, x1, x2;
+
+Console.WriteLine("Résolution de l'équation ax² + bx + c = 0.");
+Console.WriteLine("Quelle est votre coefficient a ?");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Quelle est votre coefficient b ?");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Quelle est votre coefficient c ?");
+c = Convert.ToDouble(Console.ReadLine());
+
+discriminant = (b * b) - (4 * a * c);
+
+if (discriminant < 0)
+{
+    Console.WriteLine($"L'équation {a}x² + {b}x + 2 = 0 n'a pas de solution réelle.");
+}
+else if (discriminant == 0)
+{
+    x1 = -b / (2 * a);
+    Console.WriteLine($"L'équation {a}x² + {b}x + 2 = 0 a une solution réelle {x1}");
+}
+else if (discriminant > 0)
+{
+    x1 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+    x2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+    Console.WriteLine($"L'équation {a}x² + {b}x + 2 = 0 a deux solutions réelles {x1} ou {x2}.");
+}
